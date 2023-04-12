691f203ff506b8d07a09aeaa7eb510c4b34ee149
karinasasaki <karinasasaki@github.com>
2023-04-12 08:18:22 -0400

    Validation nested form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,13 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy, inverse_of: :order
   has_many :products, through: :order_details
-  accepts_nested_attributes_for :order_details, reject_if: :all_blank, allow_destroy: true
-  
+
   belongs_to :person
 
-  validates :name, :person, presence: true
-  validates_associated :products
+  accepts_nested_attributes_for :order_details, reject_if: :all_blank, allow_destroy: true
+
+  validates :name, :person, :order_details, presence: true
+
   def total_value
       prices = products.pluck(:price)
       quantities = products.pluck(:quantity)
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,5 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
